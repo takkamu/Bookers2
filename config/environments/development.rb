093416c155a98a9b7d7e205d290e1906a53025bd
EC2 Default User <ec2-user@ip-172-31-12-242.ec2.internal>
2022-05-21 11:56:44 +0000

    [Add]ユーザ画像表示

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -75,4 +75,5 @@ Rails.application.configure do
   # config.action_cable.disable_request_forgery_protection = true
 
   config.hosts << "90b5fa1b81fb4271b1f4d65294be1c1a.vfs.cloud9.us-east-1.amazonaws.com"
+  config.active_job.queue_adapter = :inline
 end
