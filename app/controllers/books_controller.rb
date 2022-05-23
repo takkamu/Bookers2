d96d5cd0141786cc951d029b8dce0436aec96e51
EC2 Default User <ec2-user@ip-172-31-12-242.ec2.internal>
2022-05-23 11:54:21 +0000

    [Add]テスト完了

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -2,18 +2,28 @@ class BooksController < ApplicationController
   def index
     @books = Book.all
     # @book.user.name = Book.find(params[:id])
+    @user = current_user
     @book = Book.new
   end
 
   def create
     @book = Book.new(book_params)
     @book.user_id = current_user.id
-    @book.save
-    redirect_to books_path
+    if @book.save
+      redirect_to book_path(@book.id)
+      flash[:notice] = "You have created book successfully."
+    else
+      @books = Book.all
+      @user = current_user
+      render :index
+    end
+
   end
 
   def show
     @book = Book.find(params[:id])
+    @user = User.find(@book.user.id)
+    @booknew = Book.new
   end
 
   def edit
@@ -22,8 +32,12 @@ class BooksController < ApplicationController
 
   def update
     @book = Book.find(params[:id])
-    @book.update(book_params)
-    redirect_to '/books'
+    if @book.update(book_params)
+      redirect_to book_path(@book.id)
+      flash[:notice] = "You have updated book successfully."
+    else
+      render :edit
+    end
   end
 
   def destroy
@@ -32,10 +46,17 @@ class BooksController < ApplicationController
     redirect_to '/books'
   end
 
+  before_action :correct_user, only: [:edit, :update]
 
   private
 
   def book_params
     params.require(:book).permit(:title, :body)
+  end
+
+  def correct_user
+    @book = Book.find(params[:id])
+    @user = @book.user
+    redirect_to(books_path) unless @user == current_user
   end
 end
